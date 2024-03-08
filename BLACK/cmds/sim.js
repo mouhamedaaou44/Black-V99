db3c770622efb487bf605852a48bd2bc2ba8bbf0
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-03-08 18:34:09 +0100

    Update sim.js

--- BLACK/cmds/sim.js
+++ BLACK/cmds/sim.js
@@ -15,12 +15,28 @@ module.exports = {
 
     const coj = args.join(" ")
     if (!coj) return black.reply('اكتب شي')
-    const res = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=ar&message=${encodeURIComponent(coj)}&filter=false`);
-    const rd = res.data.success;
 
-
-
-    return black.reply({ body: rd }, (error, info) => {
+fetch("https://simsimi.vn/web/simtalk", {
+  "headers": {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en-GB,en;q=0.9,fr-MA;q=0.8,fr;q=0.7,en-US;q=0.6",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": "\"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Android\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest",
+    "Referer": "https://simsimi.vn/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": `text=${coj}&lc=ar&=`,
+  "method": "POST"
+}).then(async res => {
+var data = await res.json();
+  var rd = data.success;
+  return black.reply({ body: rd }, (error, info) => {
       global.client.Reply.push(info.messageID, {
         name: this.config.name,
         author: event.senderID,
@@ -28,21 +44,43 @@ module.exports = {
 
       });
     });
+});
+
+    
   },
 
   onReply: async function({ api, event, Reply, black, usersData, threadsData }) {
 
-    const res = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=ar&message=${encodeURIComponent(event.body)}&filter=false`);
-    const sim = res.data.success;
-
-
-    return black.reply({ body: sim }, (error, info) => {
+    fetch("https://simsimi.vn/web/simtalk", {
+  "headers": {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en-GB,en;q=0.9,fr-MA;q=0.8,fr;q=0.7,en-US;q=0.6",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": "\"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Android\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest",
+    "Referer": "https://simsimi.vn/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": `text=${event.body}&lc=ar&=`,
+  "method": "POST"
+}).then(async res => {
+var data = await res.json();
+  var rd = data.success;
+  return black.reply({ body: rd }, (error, info) => {
       global.client.Reply.push(info.messageID, {
         name: this.config.name,
         author: event.senderID,
         messageID: info.messageID
+
       });
     });
+});
+
 
 
 
