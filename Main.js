faec19862c95539d2d1dff40de1bb32d763ed092
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-03-10 20:39:04 +0000

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,7 +3,6 @@ const chalk = require("chalk");
 const cv = chalk.bold.hex("#FF5733");
 const gradient = require("gradient-string")
 global.BotaNota = [];
-global.game = {};
 const logo = `
 
 ╭━━╮╭╮╱╱╱╱╱╱╭╮
